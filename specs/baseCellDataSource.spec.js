39b4af816a07daa514b40578ec29b32a5b985704
Chad Autry <chad.autry@gmail.com>
2015-04-12 11:03:30 -0500

    Add test framework and dummy tests

---
+++ specs/baseCellDataSource.spec.js
@@ -0,0 +1,13 @@
+/**
+ * A set of tests for the main java file
+ */
+//Require the Constructor of the object being tested
+var BaseCellDataSource = require("../baseCellDataSource.js");
+
+describe( 'BaseCellDataSource dummy test', function() {
+
+    it( 'it should have a test', function() {
+        expect( true );
+    });
+
+});
