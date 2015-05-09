33eade839ed44fab49631525f48e52646c0dd432
Chad Autry <chad.autry@gmail.com>
2015-05-09 16:49:18 -0500

    Output had simply been hidden by default, needed to click. Seems to exit sometime after the git init

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -ev
-bundle exec rake:units
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  echo "test 1"
-fi
-
-if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
-  echo "test 2"
-fi
+set -ev;
+mkdir -p target/gh-pages;
+cp test.html ./target/gh-pages/index.html;
+cp bundle.js ./target/gh-pages/bundle.js;
+cd target/gh-pages
+git init
+git config user.name ${GIT_NAME}
+git config user.email ${GIT_EMAIL}
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
