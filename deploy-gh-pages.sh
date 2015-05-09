9426a0339416edf2e38a5837989f5b2ab459fd88
Chad Autry <chad.autry@gmail.com>
2015-05-09 15:57:26 -0500

    Build succeeding, script getting called, no deployment seen

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-rm -rf target/gh-pages || exit 0;
+echo "gather files for deployment...";
 mkdir -p target/gh-pages;
-cp test.html ./target/gh-pages/index.html
-cp bundle.js ./target/gh-pages/bundle.js
-echo "starting deploy..."
+cp test.html ./target/gh-pages/index.html;
+cp bundle.js ./target/gh-pages/bundle.js;
+echo "starting deploy...";
 ( cd target/gh-pages
  git init
  git config user.name ${GIT_NAME}
@@ -12,3 +12,4 @@ echo "starting deploy..."
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
+echo "completed deploy...";
