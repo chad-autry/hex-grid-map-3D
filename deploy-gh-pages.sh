739a459211b0e5ba915478f61d42c30a74480298
Chad Autry <chad.autry@gmail.com>
2015-05-09 16:52:35 -0500

    Had wrong variable names for Github name and email

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -5,8 +5,8 @@ cp test.html ./target/gh-pages/index.html;
 cp bundle.js ./target/gh-pages/bundle.js;
 cd target/gh-pages
 git init
-git config user.name ${GIT_NAME}
-git config user.email ${GIT_EMAIL}
+git config user.name ${GH_NAME}
+git config user.email ${GH_EMAIL}
 git add .
 git commit -m "Deployed to Github Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
