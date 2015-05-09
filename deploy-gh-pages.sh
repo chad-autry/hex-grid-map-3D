01890af6f05a0a70e413e118ebdac8eb8fe4c7e4
Chad Autry <chad.autry@gmail.com>
2015-05-09 15:49:25 -0500

    Auto deploy github-pages on successful TravisCI build

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf target/gh-pages || exit 0;
+mkdir -p target/gh-pages;
+cp test.html ./target/gh-pages/index.html
+cp bundle.js ./target/gh-pages/bundle.js
+echo "starting deploy..."
+( cd target/gh-pages
+ git init
+ git config user.name ${GIT_NAME}
+ git config user.email ${GIT_EMAIL}
+ git add .
+ git commit -m "Deployed to Github Pages"
+ git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
