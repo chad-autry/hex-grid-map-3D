368abfcbec44c1abcf36787b0174c7766a7309f9
Chad Autry <chad.autry@gmail.com>
2015-08-10 14:32:33 -0500

    A file to put JSDoc comments for Global virtual classes.

---
+++ src/Global.js
@@ -0,0 +1,17 @@
+//A place to define goball items
+
+/**
+ * A callback to be used for implementing the logic of dragging items
+ * @callback onDrag
+ * @param {integer} x - The x position of the mouse drag event
+ * @param {integer} y - The y position of the mouse drag event
+ * @param {integer} dx - The x translation since the last event
+ * @param {integer} dy - The y translation since the last event
+ */
+ 
+/**
+ * A callback to be called when the mouse is clicked on an item
+ * @callback onClick
+ * @param {integer} x - The x position of the mouse down event
+ * @param {integer} y - The y position of the mouse down event
+ */
