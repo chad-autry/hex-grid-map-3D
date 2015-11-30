c7d69aaab3d91d19ec5c9fef2b3e9b40e52367ca
Chad Autry <chad.autry@gmail.com>
2015-11-29 19:25:16 -0600

    Remove (hopefully final) references to browserifyable-paper

--- gh-pages/src/common/hexBoard/hexBoard.js
+++ gh-pages/src/common/hexBoard/hexBoard.js
@@ -1,6 +1,5 @@
 var angular = require('angular');
 var Board = require('../../../../src/HexBoard.js'); //
-var paper = require('browserifyable-paper');
 module.exports = angular.module( 'hexBoard', [] )
 
 .directive( 'hexBoard', ['$window', function($window) {
@@ -9,9 +8,6 @@ module.exports = angular.module( 'hexBoard', [] )
     
 
       scope.board = new Board(scope.hexDimensions, element[0], scope.contexts, scope.globalMouseClicked, $window);
-            onResize = function (event){
-                   paperview.viewSize = [event.size.width, event.size.height]
-        }
       scope.$emit('boardInitialized');
       
       
